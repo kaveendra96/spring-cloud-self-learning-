1d0f0b2237f1044b63a5b92d8de3965c8f328998
kaveendra96 <kaveendra96@gmail.com>
2022-02-28 10:54:34 +0530

    create Beer model

---
+++ demo003/mssc-brewery-client/src/main/java/guru/springframework/msscbreweryclient/domain/Beer.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscbreweryclient.domain;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class Beer {
+    private UUID id;
+    private Long version;
+
+    private Timestamp createdDate;
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+
+}
